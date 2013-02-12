14fc2e1ca4d569fb5395f62ce52196bc57acd0df
Jonathan Beck <jonathanbeck@gmail.com>
2013-02-12 14:12:01 -0800

    Add Pins Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,7 @@ class Pin < ActiveRecord::Base
   attr_accessible :description
 
   validates :description, presence: true, :uniqueness => { :case_sensitive => false }
+
+  belongs_to :user
+  validates :user_id, presence: true 
 end
