77f49df38f309f9586e74b7a7ac7d2dee20f496d
Jonathan Beck <jonathanbeck@gmail.com>
2013-05-01 21:55:51 -0700

    Connect Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,16 @@ Omrails::Application.configure do
   # Heroku Devise - Look at development.rb for further description. 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  # Configuring Amazon S3 to keep images uploaded for paperclip file upload, where heroku deletes them automatically
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => ENV['AWS_BUCKET'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    }
+
+end
+
 end
